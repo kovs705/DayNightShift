f272cd0d8765e2fd5dc24fcfa7496452be0b6b2b
Eugene Kovs <smartkovsik@gmail.com>
2024-02-04 17:31:23 +0300

    updated TimeTypes model

--- DayNightManager.swift
+++ DayNightManager.swift
@@ -14,14 +14,21 @@ import Foundation
     
     /// https://ru.wikipedia.org/wiki/Сумерки
     enum TimeTypes {
-        case night           // глубокая ночь
+        case night                  // глубокая ночь
         
-        case dawn            // начало рассвета, солнце под горизонтом
-        case twilightMorning // сумерки перед восходом солнца
-        case sunrise         // восход солнца
+        case astroTwilightDawn      // начало синего часа
+        case nautiTwilightDawn      // появляется полоса зари
+        case civilTwilightDawn      // начало золотого часа
+        /// case sunrise                // восход солнца
         
+        case horizon                // солнце на горизонте
         
-        
-        
+        ///case sunset                 // закат
+        case civilTwilightDusk      // ещё светло - это время принято считать сумерками
+        case nautiTwilightDusk      // уже включают городское освещение
+        case astroTwilightDusk      // на улице стемнело и горизонт практически неразличим
+        ///case dusk                   // темнота
     }
+    
+    
 }
